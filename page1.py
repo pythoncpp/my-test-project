9f67a5be60118f3c90fbeae4971802d37cdbadd2
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2022-11-30 09:57:56 +0530

    updated code with new feature

--- page1.py
+++ page1.py
@@ -1,2 +1,8 @@
 print("hello world 1")
 print("hello world 2")
+print("hello world 2")
+print("hello world 2")
+print("hello world 2")
+print("hello world 2")
+print("hello world 2")
+print("hello world 2")
